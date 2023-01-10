1ac5f3c83ca64090758329c084e747523d314073
Caleb Hester <naclcaleb@gmail.com>
2023-01-09 19:40:09 -0800

    Add scoped to style definition for CramBuilder

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,18 +1,22 @@
 <template>
     <el-card id='register-card'>
         <h2>Sign Up</h2>
+        <p style='margin-top: 10px; margin-bottom: 10px;'>So good to see you here! Let's get started transforming your learning routine!</p>
         <el-form :model='formData' label-position='top' :rules="rules" ref='theForm'>
             <el-form-item label='Email' prop='email'>
-                <el-input placeholder='Email' v-model='formData.email' :disabled='formLoading'/>
+                <el-input placeholder='Email' v-model='formData.email' :disabled='formLoading' size='large'/>
             </el-form-item>
             <el-form-item label='Password' prop='password'>
-                <el-input placeholder='Password' v-model='formData.password' type='password' :disabled='formLoading'/>
+                <el-input placeholder='Password' v-model='formData.password' type='password' :disabled='formLoading' size='large'/>
             </el-form-item>
             <el-form-item label='Confirm Password' prop='confirmPassword'>
-                <el-input placeholder='Confirm Password' v-model='formData.confirmPassword' type='password' :disabled='formLoading'/>
+                <el-input placeholder='Confirm Password' v-model='formData.confirmPassword' type='password' :disabled='formLoading' size='large'/>
+            </el-form-item>
+            <el-form-item prop='agree'>
+                <el-checkbox>I agree to the <el-link type='primary' href='https://retaincards.com/privacy-policy'>Privacy Policy</el-link> and <el-link type='primary' href='https://retaincards.com/terms-of-service'>Terms of Service</el-link>.</el-checkbox>
             </el-form-item>
             <el-form-item>
-                <el-button type='primary' @click.prevent='onFormSubmit' :loading='formLoading'>Sign Up</el-button>
+                <el-button style='width: 100%;' type='primary' @click.prevent='onFormSubmit' :loading='formLoading'>Sign Up</el-button>
             </el-form-item>
             <p>Already have an account? <el-link @click='goToLogin' type='primary'>Log In</el-link></p>
         </el-form>
@@ -50,7 +54,11 @@
                         { required: true, message: 'Please enter a password confirmation', trigger: 'blur' },
                         { validator: (rule, value, callback) => { if (value != this.formData.password) { callback(new Error('Password doesn\'t match confirmation')) } else { callback() } }, trigger: 'blur'}
                     ]
-                }
+                },
+                agree: [
+                    { required: true, message: 'You must agree to the privacy policy and terms of service.', trigger: 'blur' },
+                    { validator: (rule, value, callback) => { if (!value) { callback(new Error('You must agree to the privacy policy and terms of service.')) } else { callback() } }, trigger: 'blur'}
+                ]
             }
         },
     
