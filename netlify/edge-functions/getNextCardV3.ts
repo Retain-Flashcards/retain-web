3d2fcaa28692c44ca6f54aea6cb996f80e08da3c
Caleb Hester <naclcaleb@gmail.com>
2024-02-22 08:50:57 -0800

    Debug time markers

--- netlify/edge-functions/getNextCardV3.ts
+++ netlify/edge-functions/getNextCardV3.ts
@@ -116,6 +116,7 @@ export default async (req: Request, context: Context): Promise<Response> => {
   if (card.learning != false) {
     againTime = `${learningSteps[0]} min`
     console.log('CARD LEARNING STEP', card.learning_step)
+    console.log('GOOD TIME', (card.learning_step < learningSteps.length - 1) ? `${learningSteps[ card.learning_step + 1 ]} min`:`1 day`)
     hardTime = `${Math.round( (learningSteps[card.learning_step] + (card.learning_step < learningSteps.length - 1 ? learningSteps[card.learning_step + 1]:0))/2)} min`
     goodTime = (card.learning_step < learningSteps.length - 1) ? `${learningSteps[ card.learning_step + 1 ]} min`:`1 day`
   }
