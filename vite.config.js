936ad608835eaa3f73694929ad31706b4867b19a
Caleb Hester <naclcaleb@gmail.com>
2024-02-27 15:24:26 -0800

    Update sentry

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,9 @@ export default defineConfig({
   plugins: [vue(), vueJsx(), sentryVitePlugin({
     org: "individual-ik",
     project: "javascript-vue"
+  }), sentryVitePlugin({
+    org: "individual-ik",
+    project: "javascript-vue"
   })],
   resolve: {
     alias: {
