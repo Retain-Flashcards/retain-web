379557290bff6ba65db772dd72df09fced002995
Caleb Hester <naclcaleb@gmail.com>
2024-04-21 21:03:00 -0700

    Add log

--- netlify/edge-functions/studyCard.ts
+++ netlify/edge-functions/studyCard.ts
@@ -38,7 +38,7 @@ export default async (req: Request, context: Context): Promise<Response> => {
     { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
   )
 
-  console.log(supabase.auth.getUser())
+  console.log(await supabase.auth.getUser())
 
   const {
     data: { user },
